8d57bfd5e6d9a73a0b1844ad440e21170f5d7328
Roger Goldfinger <rgoldfinger@gmail.com>
2016-04-10 23:56:03 -0700

    simple bart handler working

--- src/sir/simpleBart.clj
+++ src/sir/simpleBart.clj
@@ -3,22 +3,19 @@
   (:require [org.httpkit.client :as http]
             [clojure.xml :as xml]
             [clojure.core.cache :as cache]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [sir.bart :as bart]))
 
 (defn parse [s]
   (xml/parse
     (java.io.ByteArrayInputStream. (.getBytes s))))
 
-(def bart-cache (atom (cache/ttl-cache-factory {} :ttl (* 1000 60))))
-
-(defn gen-trips [times trip]
-  (map
-    (fn [minutes]
-      ; minutes is either a number or "Leaving"
-      (if (number? (read-string minutes))
-        (into trip {:departureTime (+ (System/currentTimeMillis) (* (read-string minutes) 1000 60))})
-        (into trip {:departureTime (+ (System/currentTimeMillis) (* 1000 60))})))
-    times))
+; {:tag :abbreviation, :attrs nil, :content [DALY]}
+(defn get-code-from-etd [etd]
+  (let [abbreviation (filter
+                        #(= (:tag %) :abbreviation)
+                        (:content etd))]
+    (get-in (nth abbreviation 0) [:content 0 ])))
 
 (defn get-minutes-from-etd [etd]
   (map
@@ -27,36 +24,31 @@
       #(= (:tag %) :estimate)
       (:content etd))))
 
-(defn get-etd-for-eol [body]
-  (nth
-         (->>
-          body
-          xml-seq
-          (filter #(= (:tag %) :etd)))
-        0
-        ""))
+(defn get-etds [body]
+  (->>
+    body
+    xml-seq
+    (filter #(= (:tag %) :etd))))
 
 (defn get-departure-times [body]
-  (do
-    (println (get-etd-for-eol body))
-    (get-minutes-from-etd (get-etd-for-eol body))))
-
-
-(defn process-data [body]
-  (get-departure-times body))
+  (map
+    (fn [etd]
+      { :code (get-code-from-etd etd)
+      :departures (get-minutes-from-etd etd)})
+    (get-etds body)))
 
 (defn build-url
   [originStationCode]
   (str "http://api.bart.gov/api/etd.aspx?cmd=etd&orig="
        originStationCode
-       "&key=" (or (System/getenv "BART_API") "ZELI-U2UY-IBKQ-DT35")))
+       "&key=" (System/getenv "BART_API") "ZELI-U2UY-IBKQ-DT35"))
 
 (defn fetch [{originStationCode :origin}]
   (let [url (build-url originStationCode)
-        data (if (cache/has? @bart-cache originStationCode)
-               (cache/lookup @bart-cache originStationCode)
+        data (if (cache/has? @bart/bart-cache originStationCode)
+               (cache/lookup @bart/bart-cache originStationCode)
                (let [fresh-data @(http/get url)]
-                 (swap! bart-cache assoc originStationCode fresh-data)
+                 (swap! bart/bart-cache assoc originStationCode fresh-data)
                  fresh-data))]
     (let [{body :body error :error} data]
       (if error
@@ -65,65 +57,6 @@
              :body "bart api error"})
         (do
           (println (build-url originStationCode))
-          ; (into [] (process-data (parse body))))))))
           {:status 200
             :headers {"Content-Type" "application/json"}
-            :body (generate-string (process-data (parse body)))})))))
-
-(def station-data {
-  :12th-St-Oakland-City-Center "12th"
-  :16th-St-Mission "16th"
-  :19th-St-Oakland "19th"
-  :24th-St-Mission "24th"
-  :Ashby "ashb"
-  :Balboa-Park "balb"
-  :Bay-Fair "bayf"
-  :Castro-Valley "cast"
-  :Civic-Center-UN-Plaza "civc"
-  :Coliseum-Oakland-Airport "cols"
-  :Colma "colm"
-  :Concord "conc"
-  :Daly-City "daly"
-  :Downtown-Berkeley "dbrk"
-  :Dublin-Pleasanton "dubl"
-  :El-Cerrito-del-Norte "deln"
-  :El-Cerrito-Plaza "plza"
-  :Embarcadero "embr"
-  :Fremont "frmt"
-  :Fruitvale "ftvl"
-  :Glen-Park "glen"
-  :Hayward "hayw"
-  :Lafayette "lafy"
-  :Lake-Merritt "lake"
-  :MacArthur "mcar"
-  :Millbrae "mlbr"
-  :Montgomery-St "mont"
-  :North-Berkeley "nbrk"
-  :North-Concord-Martinez "ncon"
-  :Oakland-Intl-Airport "oakl"
-  :Orinda "orin"
-  :Pittsburg-Bay-Point "pitt"
-  :Pleasant-Hill "phil"
-  :Powell-St "powl"
-  :Richmond "rich"
-  :Rockridge "rock"
-  :San-Bruno "sbrn"
-  :San-Francisco-Intl-Airport "sfia"
-  :San-Leandro "sanl"
-  :South-Hayward "shay"
-  :South-San-Francisco "ssan"
-  :Union-City "ucty"
-  :Walnut-Creek "wcrk"
-  :West-Dublin "wdub"
-  :West-Oakland "woak" })
-
-(defn normalize-name [string]
-  (-> string
-      (str/replace " Station" "")
-      (str/replace " " "-")
-      (str/replace "St." "St")
-      (str/replace  "/" "-")
-      (str/replace  "'" "")))
-
-(defn station-lookup [station-name]
-  (get station-data (keyword (normalize-name station-name))))
+            :body (generate-string (get-departure-times (parse body)))})))))
